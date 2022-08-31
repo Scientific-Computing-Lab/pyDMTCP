b3f2f8a6e3a23ea018742f4fd755a3481861c3eb
gabid <gabibox@gmail.com>
2022-08-31 08:44:55 +0300

    First commit of DMTCP

---
+++ node_db_creator.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+import re
+
+
+def get_node_list():
+    cmd = "sinfo"
+    ret = subprocess.run(cmd, capture_output=True, shell=True)
+    node_list_info = (ret.stdout.decode().split('\n'))
+    sep = re.compile('[\s]+')
+    for line in node_list_info:
+        # print(line)
+        #print(sep.split(line))
+        print(line.split(' ')[-1])
+
+
+get_node_list()

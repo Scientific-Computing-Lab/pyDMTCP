5cc031deefc09a21ca961194a78980b2b64794cf
gabid <gabibox@gmail.com>
2022-09-15 11:01:43 +0300

    Get available partitions updated

--- node_db_creator.py
+++ node_db_creator.py
@@ -3,15 +3,25 @@ import subprocess
 import re
 
 
-def get_node_list():
-    cmd = "sinfo"
-    ret = subprocess.run(cmd, capture_output=True, shell=True)
+def get_avail_partitions():
+    cmd1 = "sinfo"
+    ret = subprocess.run(cmd1, capture_output=True, shell=True)
+    cmd2 = "sacctmgr list associations where user=$USER format=User,MaxJobs,Partition,GrpCPUs"
+    ret = subprocess.run(cmd2, capture_output=True, shell=True)
+    # print(ret.stdout.decode())
     node_list_info = (ret.stdout.decode().split('\n'))
     sep = re.compile('[\s]+')
+    partitions = []
     for line in node_list_info:
-        # print(line)
-        print(sep.split(line))
-        print(line.split(' ')[-1])
+        part_values = ([x for x in sep.split(line) if len(x) > 1])
+        if len(part_values) > 0:
+            partitions.append(part_values[-2])
+
+    partitions = partitions[2:]  # remove the header
+    return partitions
+
+def get_avail_nodes():
+    cmd =
 
 
-get_node_list()
+get_avail_partitions()
